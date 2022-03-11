85598a18428d902120669debc0833f550e101e09
inobelieve <514396432@qq.com>
2022-03-11 17:42:20 +0800

    feat：整体引入

--- src/index.js
+++ src/index.js
@@ -8,33 +8,29 @@
 import nhButton from './nh-button/index.vue';
 import nhInput from './nh-input/index.vue';
 
-const components = {
+function install(component) {
+  component.install = Vue => Vue.component(component.name, component);
+}
+
+install(nhButton)
+install(nhInput)
+
+const components = [
+  nhButton,
+  nhInput
+]
+
+const all = {
+  install(Vue) {
+    components.forEach(component => {
+      Vue.component(component.name, component);
+    })
+  }
+}
+let nhUI = {}
+Object.assign(nhUI, all, installedComponents)
+export {
+  all,
   nhButton,
   nhInput
 }
-function install(component) {
-  component.install = Vue => Vue.component(component.name, component);
-  return component
-}
-const installedComponents = ((components) => {
-  let installed = {}
-  for (const name in components) {
-    let component = components[name]
-    component.install = (Vue) => {
-      Vue.component(component.name, component);
-    }
-    installed[name] = component
-  }
-  return installed
-})(components)
-
-const nhUI = {
-  install(Vue) {
-    for (const name in components) {
-      const component = components[name]
-      Vue.component(component.name, component);
-    }
-  }
-}
-Object.assign(nhUI, installedComponents)
-export default nhUI
