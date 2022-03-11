da68a7e16c4e1d8bc36cf15e432a772363bf66f0
inobelieve <514396432@qq.com>
2022-03-11 14:02:42 +0800

    feat：更换框架模模板

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ui-for-newhope
-this is a UI project for New Hope Group company's business
+
+> this is a UI project for New Hope Group company's business
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/App.vue
+++ src/App.vue
@@ -8,23 +8,23 @@
 
 <script>
   import nhButton from './nh-button'
-export default {
-  name: 'app',
-  components: {nhButton},
-  data () {
-    return {
+  export default {
+    name: 'app',
+    components: {nhButton},
+    data () {
+      return {
+      }
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
