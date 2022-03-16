42eb10d2e9536d957b3a69d74c081b29c24d76c2
inobelieve <514396432@qq.com>
2022-03-16 17:57:22 +0800

    fix: install fixed

--- src/packages/index.js
+++ src/packages/index.js
@@ -5,15 +5,16 @@
  * @LastEditTime: 17:10:06
  */
 import nhButton from './nh-button'
+import nhButtonVue from './nh-button/index.vue'
 import nhInput from './nh-input'
+import nhInputVue from './nh-input/index.vue'
 
-const components = [nhButton ,nhInput]
-const nhUI = {
-    install (Vue) {
-        components.forEach(item => {
-            Vue => Vue.component(item.name, item);
-        })
-    }
+const components = [nhButtonVue ,nhInputVue]
+let nhUI = {}
+nhUI.install = (Vue) => {
+    components.forEach(item => {
+        Vue => Vue.component(item.name, item);
+    })
 }
 
 export default {
