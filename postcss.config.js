087d78f5119ad0da8e8b6ce6187922cc64776f39
inobelieve <514396432@qq.com>
2022-03-16 15:57:10 +0800

    测试分支

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+/**
+ * @Description: vue config文件
+ * @Author: inobelieve by WebStorm
+ * @Date: 2022-03-15
+ * @LastEditTime: 13:39:42
+ */
+module.exports = {
+    plugins: [
+        require('autoprefixer')
+    ]
+}
