dbf80483199b69c143b59e7cb616874748bf600a
inobelieve <514396432@qq.com>
2022-03-16 16:02:01 +0800

    测试分支初始化

---
+++ src/packages/nh-button/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="nh-button">
+      <el-button :type="type" @click="btnClick">
+        <slot name="default">暂无名字</slot>
+      </el-button>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+    import {Button} from 'element-ui'
+    Vue.use(Button)
+    export default {
+      name: 'nh-button',
+      props: {
+        type: {
+          type: String,
+          default: 'default',
+          required: false
+        }
+      },
+      methods: {
+        btnClick(e) {
+          this.$emit('click', e)
+        }
+      }
+    }
+</script>
+
+<style lang="scss" scoped>
+  .nh-button {
+  }
+</style>
